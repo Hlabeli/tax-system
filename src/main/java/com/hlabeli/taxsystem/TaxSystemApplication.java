1ca5d3eef4cc885c83dcf1870033283f1daf5ef0
Hlabeli <hlabeli.zee@gmail.com>
2024-01-25 21:05:55 +0200

    Initial commit

---
+++ src/main/java/com/hlabeli/taxsystem/TaxSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hlabeli.taxsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hlabeli/taxsystem/TaxSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlabeli.taxsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
